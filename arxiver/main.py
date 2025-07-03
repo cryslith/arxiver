c5e2a681b76e73728573147337efa211f24cf2c9
Lily Chung <lkdc@mit.edu>
2025-07-03 05:12:33 -0400

    add gpl license

--- arxiver/main.py
+++ arxiver/main.py
@@ -1,4 +1,20 @@
-# Adapted from D.J. Sutherland's code and modified by cryslith.
+# See AUTHORS.
+
+# Copyright (c) 2018-2020, D.J. Sutherland
+# Copyright (c) 2024-2025  cryslith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as published by
+# the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import argparse
 import io
